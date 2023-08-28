52be97521660199d959cd82e85eefffe51f25958
Sebastian Gnann <gnann@ad.umwelt.uni-potsdam.de>
2023-08-28 15:11:47 +0200

    clean up notebook and data

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
-# Budyko und die klimatische Wasserbilanz
+# Online-Vorlesung: Budyko und die klimatische Wasserbilanz
 
-Jupyter Notebook für interaktive Lehre. 
-Die Hauptdatei ist *Budyko.ipynb*. 
+!!! Achtung: dieses Notebook wird momentan noch entwickelt !!!
+
+Dieses Github-Repository enthält das Material für die interaktive Online-Vorlesung zum Thema "Budyko und die klimatische Wasserbilanz". 
+Die Hauptdatei ist *Budyko.ipynb*, ein sogenanntes Jupyter-Notebook. 
 Diese enthält das relevante Material für die Online-Vorlesung.
 
-Sie können das Notebook unter folgendem Link im Browser laufen lassen: [https://mybinder.org/v2/gh/SebastianGnann/Budyko_Lehre/HEAD](https://mybinder.org/v2/gh/SebastianGnann/Budyko_Lehre/HEAD)
+Sie können das Jupyter-Notebook unter folgendem Link im Browser laufen lassen: [https://mybinder.org/v2/gh/SebastianGnann/Budyko_Lehre/HEAD](https://mybinder.org/v2/gh/SebastianGnann/Budyko_Lehre/HEAD)
 
 Bitte beachten Sie, dass das Laden des Notebooks via MyBinder manchmal einige Minuten dauern kann.
 
+## Weitere Informationen
+
+### Inhalt des Repositories
+*load_data_CAMELS_US.py* lädt und prozessiert die Daten, die im Notebook genutzt werden.
+
+### Daten
 Die Daten basieren auf den folgenden Datensätzen
 - CAMELS US Datensatz: https://gdex.ucar.edu/dataset/camels.html & https://gdex.ucar.edu/dataset/camels.html
 - CAMELS GB Datensatz: https://catalogue.ceh.ac.uk/documents/8344e4f3-d2ea-44f5-8afa-86d2987543a9
--- load_data_CAMELS_US.py
+++ load_data_CAMELS_US.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import geopandas as gpd
 import os
 from functions import helper_fcts
 
@@ -68,6 +69,15 @@ df_mean = df_selected.groupby('gauge_id').mean()
 df_mean["aridity"] = df_mean["pet"]/df_mean["precipitation"]
 df_mean["runoff_ratio"] = df_mean["streamflow"]/df_mean["precipitation"]
 
+# calculate aridity and climatic water balance
+df_mean["aridity_class"] = 0 # assign aridity class (0 = energy-limited, 1 = water-limited)
+df_mean.loc[df_mean["aridity"] > 1, "aridity_class"] = 1
+df_mean["climatic_water_balance"] = df_mean["precipitation"] - df_mean["pet"] # climatic water balance is defined as PET - P
+
+# calculate evaporative fraction using the Budyko equation and use it to calculate Budyko anomalies
+df_mean["evaporative_fraction"] = helper_fcts.Budyko_curve(df_mean["aridity"])
+df_mean["Budyko_anomaly"] = (1 - df_mean["runoff_ratio"]) - df_mean["evaporative_fraction"]
+
 print("finished averaging time series")
 
 df_attributes["gauge_id"] = df_attributes["gauge_id"].astype(str).str.zfill(8)
@@ -90,6 +100,7 @@ for i in range(len(years)):
     df_tmp = df_tmp.add_suffix('_' + str(int(year)))
     df = df_tmp.join(df, on='gauge_id')
 
+"""
 # calculate decadal averages
 decades = [['1980','1989'], ['1990','1999'], ['2000','2009']]
 for decade in decades:
@@ -101,6 +112,7 @@ for decade in decades:
     df_tmp["runoff_ratio"] = df_tmp["streamflow"]/df_tmp["precipitation"]
     df_tmp = df_tmp.add_suffix('_' + decade[0] + '-' + decade[1])
     df = df_tmp.join(df, on='gauge_id')
+"""
 
 # save data
 df.to_csv(results_path + 'camels_US_processed.csv', index=False)
@@ -124,3 +136,17 @@ helper_fcts.plot_Budyko_curve(np.linspace(0,10,100), axes)
 fig.savefig(figures_path + x_name + '_' + y_name + "_CAMELS_US" + ".png", dpi=600, bbox_inches='tight')
 plt.close()
 
+# plot map
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+#us = world[world.name == 'United States of America']
+scatter_df = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.gauge_lon, df.gauge_lat))
+fig, ax = plt.subplots(figsize=(6, 3))
+world.boundary.plot(ax=ax, linewidth=0.5, color='black')
+world.plot(ax=ax, color='lightgrey', edgecolor='black')
+scatter_df.plot(ax=ax, markersize=10, color='tab:blue')
+plt.title('CAMELS US Einzugsgebiete')
+plt.xlabel('Längengrad')
+plt.ylabel('Breitengrad')
+plt.xlim(-130, -65); plt.ylim(20, 55)
+fig.savefig(figures_path + "map_CAMELS_US" + ".png", dpi=600, bbox_inches='tight')
+plt.close()
